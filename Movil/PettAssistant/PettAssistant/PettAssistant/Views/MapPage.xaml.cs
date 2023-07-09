b781ce06ff0aae44e7ef493102e032002ec2faf5
alexandro157 <alexandro.choque@tecsup.edu.pe>
2023-07-09 03:09:32 -0500

    commit-mapa
    se implemento el api de google maps

--- Movil/PettAssistant/PettAssistant/PettAssistant/Views/MapPage.xaml.cs
+++ Movil/PettAssistant/PettAssistant/PettAssistant/Views/MapPage.xaml.cs
@@ -4,9 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using Xamarin.Forms.GoogleMaps;
 
 namespace PettAssistant.Views
 {
@@ -15,6 +14,31 @@ namespace PettAssistant.Views
         public MapPage()
         {
             InitializeComponent();
+
+            Pin MiUbi = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "Mi Ubicacion",
+                Address = "Av. Globo Terráqueo 3512, San Martín de Porres 15311",
+                Position = new Position(-12.000521750624953, -77.06209627111522),
+                Tag = "id_miubi",
+            };
+
+            Pin MiUbi2 = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "Mi Ubicacion",
+                Address = "Av. Globo Terráqueo 3512, San Martín de Porres 15311",
+                Position = new Position(-12.000663, -77.061356),
+                Tag = "id_miubi",
+                
+            };
+
+            map.Pins.Add(MiUbi);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(MiUbi.Position, Distance.FromMeters(500)));
+
+            map.Pins.Add(MiUbi2);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(MiUbi2.Position, Distance.FromMeters(500)));
         }
     }
 }
