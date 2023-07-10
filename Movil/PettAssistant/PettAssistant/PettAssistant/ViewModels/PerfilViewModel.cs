b434c559b8632535d7b7ad46a31c8f8ae07e8ba2
alexandro157 <alexandro.choque@tecsup.edu.pe>
2023-07-09 23:42:23 -0500

    commit-perfil
    Se agrego la vista perfil

--- Movil/PettAssistant/PettAssistant/PettAssistant/ViewModels/PerfilViewModel.cs
+++ Movil/PettAssistant/PettAssistant/PettAssistant/ViewModels/PerfilViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PettAssistant.Views;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
@@ -6,14 +7,41 @@ using Xamarin.Forms;
 
 namespace PettAssistant.ViewModels
 {
-    public class PerfilViewModel:BaseViewModel
+    public class PerfilViewModel : BaseViewModel
     {
-        public Command PerfilCommand { get; }
+        public ICommand NavegarCommand { get; set; }
+        public ICommand NavegarCommand1 { get; set; }
+        public ICommand NavegarCommand2 { get; set; }
+        public ICommand NavegarCommand3 { get; set; }
         public PerfilViewModel()
         {
             Title = "Perfil";
-        }
+            NavegarCommand = new Command(Navegar);
+            NavegarCommand1 = new Command(Navegar1);
+            NavegarCommand2 = new Command(Navegar2);
+            NavegarCommand3 = new Command(Navegar3);
 
-        public ICommand OpenWebCommand { get; }
+        }
+        
+        private void Navegar()
+        {
+            // Aquí realizas la navegación hacia la otra vista
+            Application.Current.MainPage.Navigation.PushAsync(new InicioPage());
+        }
+        private void Navegar1()
+        {
+            // Aquí realizas la navegación hacia la otra vista
+            Application.Current.MainPage.Navigation.PushAsync(new MenuPage());
+        }
+        private void Navegar2()
+        {
+            // Aquí realizas la navegación hacia la otra vista
+            Application.Current.MainPage.Navigation.PushAsync(new AyudaPage());
+        }
+        private void Navegar3()
+        {
+            // Aquí realizas la navegación hacia la otra vista
+            Application.Current.MainPage.Navigation.PushAsync(new MapPage());
+        }
     }
 }
